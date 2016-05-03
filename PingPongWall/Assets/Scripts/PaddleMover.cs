9f3d839037b0ee29178eb1070a87f3c6ce0a3bfd
radbart <pacrad@gmail.com>
2016-05-03 00:02:19 -0400

    Ping Pong Wall
    [ADD] Unit Project (table, wall, boucing ball and paddle)

---
+++ PingPongWall/Assets/Scripts/PaddleMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMover : MonoBehaviour {
+
+    public int paddleForce;
+
+    void Start()
+    {
+        Debug.Log("Game Start");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Paddle hit!");
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(0, 4, paddleForce, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
